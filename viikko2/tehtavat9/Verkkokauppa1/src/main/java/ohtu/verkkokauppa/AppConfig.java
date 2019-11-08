f81b76a592ef12e778a5fc4e468f2329fa2fc32b
Riku Honkanen <riku.honkanen9595@gmail.com>
2019-11-08 19:11:22 +0200

    kaikki valmista

---
+++ viikko2/tehtavat9/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/AppConfig.java
@@ -0,0 +1,10 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ohtu.verkkokauppa")
+public class AppConfig {
+
+}
